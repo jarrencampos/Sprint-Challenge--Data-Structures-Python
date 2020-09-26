e11fbde272f9c115ae636715b4f49fbf33ddbfae
Jarren <jarrenmcampos@gmail.com>
2020-09-25 20:57:57 -0700

    Finished names.py

--- names/names.py
+++ names/names.py
@@ -1,22 +1,29 @@
 import time
+import os
 
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
+cwd = os.getcwd()
+f = open(f'{cwd}/names/names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-f = open('names_2.txt', 'r')
+f = open(f'{cwd}/names/names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+with open(f'{cwd}/names/names_1.txt') as f1, open(f'{cwd}/names/names_2.txt') as f2:
+    for line in set(line.strip() for line in f1) & set(line.strip() for line in f2):
+        if line:
+            duplicates.append(line)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
